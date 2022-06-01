415ff3ecc204df5aa33beaa74c5706c12b00c614
Jesus David Barrios <jesusdavid444@gmail.com>
2022-06-01 14:31:11 -0500

    Filter and pagination implementation

--- astroPics/astroPics/settings.py
+++ astroPics/astroPics/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'filters',
     'astroPics_rest_API'
 ]
 
--- astroPics/astroPics_rest_API/views.py
+++ astroPics/astroPics_rest_API/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
+
+from filters.mixins import FiltersMixin
 
 from .serializers import *
 from .models import *
 
-class AstronomyPictureView(viewsets.ModelViewSet):
+class PaginationConfig(PageNumberPagination):
+    page_size = 12
+    page_size_query_param = 'page_size'
+    max_page_size = 25
+
+class AstronomyPictureView(FiltersMixin, viewsets.ModelViewSet):
     queryset = AstronomyPicture.objects.all()
     serializer_class = AstronomyPictureSerializer
+    pagination_class = PaginationConfig
+    
+    filter_mappings = {
+        'title': 'title__icontains',
+        'explanation': 'explanation__icontains'
+    }
+    
